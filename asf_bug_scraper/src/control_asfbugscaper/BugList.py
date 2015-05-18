513781e2bda26838f8e2bf4e3d75a5c1274dc9a0
Vagner Clementino <vagner.clementino@gmail.com>
2015-05-17 21:08:53 -0300

    [AMOR] Iniciando a criação da classe BugList

--- asf_bug_scraper/src/control_asfbugscaper/BugList.py
+++ asf_bug_scraper/src/control_asfbugscaper/BugList.py
@@ -7,7 +7,8 @@ from control_asfbugscaper.ASFBugScraperError import ASFBugScraperError
 
 class BugList(object):
     '''
-    classdocs
+        Classe para representar uma lista dos IDS dos bugs.
+        A partir destes ID's será possível recuperar as informações dos Bugs
     '''
 
 
@@ -20,20 +21,57 @@ class BugList(object):
             __isEmpty = True
             __numberOfItems = 0
             __bugListModel = None
+            __nextItem = 0
         except Exception as e:
             raise ASFBugScraperError("Erro ao criar o objeto BugList. Detalhes: {0}".format(e.message))
         
     def retrieveAllBugs(self):
+        '''
+            Retorna a lista completa de bugs
+        '''
         None
-       
+    #endDef   
     def length(self):
+        '''
+            Retorna o tamanho da lista de bugs ID
+        '''
         return self.____numberOfItems
-   
+    #endDef
     def isEmpty(self):
+        '''
+            Retorna o TRUE se a lista estiver vazio, FALSE caso contrário
+        '''
         return self.__isEmpty
-    
-    def getAllBugs(self):
-        None
+    #endDef  
+
+    def getNextBugID(self):
+        '''
+            Retorna o proximo ID de bug  da lista
+        '''
+        if len(not self.__isEmpty) > 0:
+            
+            return self.__buglist[self.__nextItem]
         
-    def getBug(self, an_bug_id):
-        None
+        else:
+                
+            raise ASFBugScraperError("Erro: a lista está vazia")
+        
+        #endIf         
+    #endDef
+    
+    def hasMore(self):
+        '''
+            Returna TRUE se existe mais algum item da lista para ser recuperado
+            ou FALSE caso contrário
+        '''
+        hasMore = False
+        if(self.isEmpty()):
+            hasMore = False
+        elif(0 <= self.__nextItem and self.length()):
+            hasMore = True
+        else:
+            hasMore = False
+            
+        #endIf
+        return hasMore        
+    #EndDef          
---
+++ asf_bug_scraper/src/model_asfbugscaper/BugListModel.py
@@ -0,0 +1,17 @@
+'''
+Created on 17/05/2015
+
+@author: vagner
+'''
+
+class BugListModel(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
